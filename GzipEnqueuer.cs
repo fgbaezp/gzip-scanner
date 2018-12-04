e1db523be751c1ff716b9ac6ca9c38ebaa7c4227
Federico Gerardo Rafael Báez Petrocelli <fgbaezp@gmail.com>
2018-12-04 15:48:35 -0300

    Added classes for enable different actions with the list of directories

---
+++ GzipEnqueuer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace gzip
+{
+	public class GzipEnqueuer : IActor
+	{
+		private readonly string queueName = "gzip";
+		private readonly CloudStorageAccount account;
+
+		public GzipEnqueuer(CloudStorageAccount account)
+		{
+			account = account ?? throw new ArgumentNullException(nameof(account));
+			var queueClient = account.CreateCloudQueueClient();
+			var queueRef = queueClient.GetQueueReference(queueName);
+			queueRef.CreateIfNotExistsAsync().Wait();
+		}
+
+		public async Task Act(string content)
+		{
+			var queueClient = account.CreateCloudQueueClient();
+			var queue = queueClient.GetQueueReference("gzip");
+			var message = new CloudQueueMessage(content);
+			await queue.AddMessageAsync(message);
+		}
+	}
+}
---
+++ IActor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gzip
+{
+	interface IActor
+	{
+		Task Act(string content);
+	}
+}
